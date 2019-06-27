cafd2cc5fcf99563762b07a45bae91115b9105ae
Jeong Hyeon <rhea31@naver.com>
2019-06-27 15:36:19 +0900

    Update server.js

--- Source/Discord/server.js
+++ Source/Discord/server.js
@@ -32,7 +32,7 @@ module.exports = (token, port, startletter) => {
                 delete require.cache[require.resolve(`./function/${array[0].split(startletter)[1]}.js`)];
                 (require(`./function/${array[0].split(startletter)[1]}.js`))(message, array, client, embed, port, ()=>{});
             } catch(e) {
-                console.log(e)
+                //console.log(e)
             }
         }
     });

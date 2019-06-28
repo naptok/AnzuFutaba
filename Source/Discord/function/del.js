ffe300819586691f2b23323ad11251b7a023d2c5
Jeong Hyeon <rhea31@naver.com>
2019-06-28 09:39:34 +0900

    sync

--- Source/Discord/function/del.js
+++ Source/Discord/function/del.js
@@ -16,25 +16,35 @@ module.exports = (message, array, client, embed, port, back) => {
     
     request.post(options, (err, res, body) => {
         if (body.success) {
-            delete_containers(body.string, back);
-            //send_string = `${message.author} **200 OK**\n\`\`\`\n${send_string}\n\`\`\``
+            var lists = data.split("\n");
+            var running_cycle = {index:-1, count:0, list:[]};
+            for(var index in lists) {
+                if (!(lists[index] == "")) {
+                    if(lists[index].indexOf(array[1]) > -1) {
+                        running_cycle.count++;
+                        running_cycle.list.push(lists[index].split(":")[1]);
+                    }
+                }
+            }
+            
+            delete_containers(running_cycle);
         }else{
             message.channel.send(`${message.author} **500 ERROR**\n\`\`\`\n${body.reason.message}\n\`\`\``);
             back();
         }
     });
 
-    function delete_containers(data, back){
-        // delete containers
-        var lists = data.split("\n");
-        for(var index in lists) {
-            if (!(lists[index] == "")) {
-                if(lists[index].indexOf(array[1]) > -1){
-                    var _array = ["ps", lists[index].split(":")[1]];
-                    (require(`./rm.js`))(message, _array, client, embed, port, ()=>{});
-                }
+    function delete_containers(data) {
+        data.index++;
+        if(data.index < data.count) {
+            try {
+                var _array = ["ps", data.list[data.index]];
+                (require(`./rm.js`))(message, _array, client, embed, port, ()=>{  });
+            }catch(e){
+                message.channel.send(data.list[data.index]);
             }
+        }else {
+            back();
         }
-        back();
     }
 }
